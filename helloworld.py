76b5ed4ff9958438d742ddb0bf09a1f584fa8b31
zhaoyea <tanzhaoyea98.tzy@gmail.com>
2019-01-24 11:36:52 +0800

    dataframe fully displayed

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os, re
+import multiprocessing as mp
 
 path = "D:/SIT/ICT-1002 Programming Fundamentals/ICT1002_Tinder/profile/"
 
@@ -9,8 +10,9 @@ path = "D:/SIT/ICT-1002 Programming Fundamentals/ICT1002_Tinder/profile/"
 def file_handler():
     files = [file for file in os.listdir(".\profile") if file.endswith(".txt")]
 
-    HEADERS = ['Name', 'Gender', 'Country', 'Acceptable_country', 'Age', 'Acceptable_age_range', 'Likes', 'Dislikes', 'Books']
-    df = pd.DataFrame([])
+    HEADERS = ['Name', 'Gender', 'Country', 'Acceptable_country', 'Age', 'Acceptable_age_range', 'Likes', 'Dislikes',
+               'Books']
+    DATA = []
 
     for file in files:
         filename = path + file
@@ -19,16 +21,15 @@ def file_handler():
         lines = text_file.read()
         text_file.close()
 
-        books = re.search(
-            r"(Name):(.*)\n+(Gender):(.*)\n+(Country):(.*)\n+(Acceptable_country):(.*)\n+(Age):(.*)\n+(Acceptable_age_range):(.*)\n+(Likes):(.*)\n+(Dislikes):(.*)\n+(Books):((?<=Books:)\D+)",
-            lines)
+        books = re.search("(Name):(.*)\n+(Gender):(.*)\n+(Country):(.*)\n+(Acceptable_country):(.*)\n+(Age):(.*)\n+(Acceptable_age_range):(.*)\n+(Likes):(.*)\n+(Dislikes):(.*)\n+(Books):((?<=Books:)\D+)",lines)
 
-        DATA = [books.group(i) for i in range(len(books.groups()) + 1) if not i % 2 and i != 0]  # retreieve row values
+        DATA.append([books.group(i).strip() for i in range(len(books.groups()) + 1) if not i % 2 and i != 0])
 
-        data = pd.DataFrame([DATA], columns=HEADERS)
-        data = data.transpose()
-        df = df.append(data)
-    print df
+    df = pd.DataFrame(DATA, columns=HEADERS)
+    df = df.replace('\n', ',', regex=True)
+    # print df
+    return df
+
 
 def main():
     print "Hello World"
