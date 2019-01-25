6592e3b61c9af62d57f1ee511c3b83bea290a158
zhaoyea <tanzhaoyea98.tzy@gmail.com>
2019-01-26 00:52:12 +0800

    fuction 2 done

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import function1 as f1
-
+import function2_test as f2
 
 if __name__ == '__main__':
-    df = f1.LOAD_PROFILES(f1.FILE_HANDLER().DATA, f1.FILE_HANDLER().HEADERS)
-    print df.profilesDF["Books"]
+    df_instances = f1.LOAD_PROFILES(f1.FILE_HANDLER().DATA, f1.FILE_HANDLER().HEADERS)
+    df = df_instances.profilesDF
+    matches = f2.COUNTRY_MATCH(df, "Michael Jackson")
+    # print matches
