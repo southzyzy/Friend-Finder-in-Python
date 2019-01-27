77b76f02990acdecc1faf732279dafe370392eb2
zhaoyea <tanzhaoyea98.tzy@gmail.com>
2019-01-27 23:46:02 +0800

    formatted main.py

--- function2.py
+++ function2.py
@@ -3,5 +3,8 @@ class COUNTRY_MATCH:
         student_B_info = df.loc[df["Name"].isin([name])]
         student_B_gender = student_B_info.Gender.str
         student_B_country = student_B_info["Acceptable_country"].values[0].split(", ")
+
         condition = df['Country'].isin(student_B_country) & (df['Name'] != name) & (df['Gender'] != student_B_gender)
-        self.countries_matches = df.loc[condition].apply(lambda x : (x["Name"], x['Gender'], x['Country'], x['Rank']+1), axis=1)
+
+        self.countries_matches = df.loc[condition]
+        self.countries_matches["Rank"] +=1
--- function3.py
+++ function3.py
@@ -2,7 +2,7 @@ class LIKES:
     def __init__(self, df, name, countries_matches):
         matches_list_names = [countries_matches.values[i][0] for i in range(len(countries_matches))]
         matched_df = df.loc[df["Name"].isin(matches_list_names)]
-
+        # print matched_df
 
         # matched = [df.loc[df["Name"] == n].apply(lambda x : (x["Name"], x["Likes"], x["Dislikes"]), axis=1) for n in matches_list_names]
         # likes = [single_match.values[0][1].split(", ") for single_match in matched]
