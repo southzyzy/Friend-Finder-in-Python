a2380868f4676a171bdc3b38921857a5ffcbe4bb
Roach105 <32363441+Roach105@users.noreply.github.com>
2019-01-31 23:01:36 +0800

    Starting UI
    Just a basic simple starting UI.

---
+++ starting_ui.py
@@ -0,0 +1,6 @@
+from pyfiglet import Figlet
+font_display = Figlet(font = "starwars")
+print font_display.renderText("Welcome To Zhao Tinder")
+
+
+profiles_folder_path = raw_input("Enter Profile Folder Path: ") 
