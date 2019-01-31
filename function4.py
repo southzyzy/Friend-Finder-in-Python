f3c56afa206d4d29795763623d87466c3b4957ff
zhaoyea <tanzhaoyea98.tzy@gmail.com>
2019-01-31 11:06:42 +0800

    skeleton of function 4 done

---
+++ function4.py
@@ -0,0 +1,27 @@
+import sys
+import requests
+
+
+class G_BOOKS():
+    googleapikey = sys.argv[1]
+
+    def search(self, value):
+        parms = {"q": value, 'key': self.googleapikey}
+        r = requests.get(url="https://www.googleapis.com/books/v1/volumes", params=parms)
+        print r.url
+        rj = r.json()
+        print rj["totalItems"]
+        for i in rj["items"]:
+            try:
+                print repr(i["volumeInfo"]["description"])
+            except:
+                pass
+            try:
+                print repr(i["volumeInfo"]["imageLinks"]["thumbnail"])
+            except:
+                pass
+
+
+if __name__ == "__main__":
+    bk = G_BOOKS()
+    bk.search("Christian reflections")
