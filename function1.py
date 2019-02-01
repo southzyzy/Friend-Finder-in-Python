bab96b113281a4bf013e74be45220c9130b90c88
zhaoyea <tanzhaoyea98.tzy@gmail.com>
2019-02-01 11:52:18 +0800

    change format of function 1

--- function1.py
+++ function1.py
@@ -1,24 +1,24 @@
 import pandas as pd
-import os, re
-
-CURENT_DIR = os.path.dirname(__file__)  # specify current directory
-PROFILES = os.path.join(CURENT_DIR, "profile/")  # locate the data profile
-files = [file for file in os.listdir(PROFILES) if file.endswith(".txt")]  # list out all the filename in profiles folder
+import re
 
 
 def formattingData(df):
-    # replace F to Female
-    # print df.Gender.str.contains("F")
-    df["Gender"] = df.Gender.str.replace("F", "Female")
+    # Check Gender column
+    if df.Gender.str.contains("F").any():
+        df["Gender"] = df.Gender.str.replace("F", "Female")
+    if df.Gender.str.contains("M").any():
+        df["Gender"] = df.Gender.str.replace("M", "Male")
+
     df["Acceptable_country"] = df.Acceptable_country.str.replace(", ", ",")  # remove the white space after ,
     df["Likes"] = df.Likes.str.replace(", ", ",")  # remove white spaces after ,
     df["Dislikes"] = df.Dislikes.str.replace(", ", ",")  # remove white spaces after ,
     df = df.replace('\n', ',', regex=True)  # replace all the \n in Books to ','
+
     return df
 
 
-class FILE_HANDLER:
-    def __init__(self):
+class FUNCTION_1(object):
+    def __init__(self, files, profiles):
         # specify the column header
         self.HEADERS = ['Name', 'Gender', 'Country', 'Acceptable_country', 'Age', 'Acceptable_age_range', 'Likes',
                         'Dislikes', 'Books']
@@ -26,7 +26,7 @@ class FILE_HANDLER:
         self.DATA = []  # create the empty list to store profiles
 
         for file in files:  # iterate over each file
-            filename = PROFILES + file  # full path name of the data files
+            filename = profiles + file  # full path name of the data files
 
             text_file = open(filename, "r")  # open the file
             lines = text_file.read()  # read the file in memory
@@ -44,9 +44,8 @@ class FILE_HANDLER:
             self.DATA.append([profiles_data.group(i).strip() for i in range(len(profiles_data.groups()) + 1) if
                               not i % 2 and i != 0])
 
-
-class PROFILES_DF:
-    def __init__(self, data, headers):
-        self.profilesDF = pd.DataFrame(data, columns=headers)  # create the dataframe
-        self.profilesDF = formattingData(self.profilesDF)  # format and make data nicer
-        self.profilesDF["Rank"] = 0.0  # create a column Rank to rank the matches
+    def profilesDF(self, headers, data):
+        df = pd.DataFrame(data, columns=headers)  # create the dataframe
+        df = formattingData(df)  # format and make data nicer
+        df["Rank"] = 0.0  # create a column Rank to rank the matches
+        return df
--- main.py
+++ main.py
@@ -1,33 +1,32 @@
-import warnings, time
+import warnings, time, os
+# import function1 as f1
 import function1 as f1
 import function2 as f2
 import function3 as f3
 import student_B as sb
 
-
-def function1():
-    """ This part serves function 1 """
-    df_instances = f1.PROFILES_DF(f1.FILE_HANDLER().DATA, f1.FILE_HANDLER().HEADERS)
-    df = df_instances.profilesDF
-    return df
+CURENT_DIR = os.path.dirname(__file__)  # specify current directory
+PROFILES = os.path.join(CURENT_DIR, "profile/")  # locate the data profile
+files = [file for file in os.listdir(PROFILES) if file.endswith(".txt")]  # list out all the filename in profiles folder
 
 
 def main():
     """ This part serves function 1 """
-    main_df = function1()
+    profiles_list = f1.FUNCTION_1(profiles=PROFILES, files=files)
+    profiles_df = profiles_list.profilesDF(profiles_list.HEADERS, profiles_list.DATA)
 
     """ Getting student B information """
     student_B_name = "Tyesha Dicus"
-    student_B_info = sb.STUDENT_B(main_df, student_B_name).student_B_info
+    student_B_info = sb.STUDENT_B(profiles_df, student_B_name).student_B_info
 
     """ This part serves function 2 """
-    f2_matches = f2.COUNTRY_MATCH(main_df, student_B_name, student_B_info).countries_matches
+    f2_matches = f2.COUNTRY_MATCH(profiles_df, student_B_name, student_B_info).countries_matches
 
     """ This part serves function 3 """
-    f3_matches = f3.LIKES_DISLIKES(f2_matches) # calling the class LIKES_DISLIKES
-    f3_matches_lst = f3_matches.convert2List() #converting dataframe to list
-    countLikes = f3_matches.countMatch(f3_matches_lst, student_B_info, "Likes") # count the no. of likes
-    countDislikes = f3_matches.countMatch(f3_matches_lst, student_B_info, "Dislikes") # count the no. of dislikes
+    f3_matches = f3.LIKES_DISLIKES(f2_matches)  # calling the class LIKES_DISLIKES
+    f3_matches_lst = f3_matches.convert2List()  # converting dataframe to list
+    countLikes = f3_matches.countMatch(f3_matches_lst, student_B_info, "Likes")  # count the no. of likes
+    countDislikes = f3_matches.countMatch(f3_matches_lst, student_B_info, "Dislikes")  # count the no. of dislikes
     end = f3_matches.matches(countLikes, countDislikes, f3_matches_lst)
     print end
 
