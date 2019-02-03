15a62e9ea56d821c9b4f95c5bcbc7dc5ad0c92dc
kohjie <1802967@sit.singaporetech.edu.sg>
2019-02-03 20:10:25 +0800

    Update to UI for f3

--- ui.py
+++ ui.py
@@ -16,21 +16,26 @@ def display_ui():
     print ui_banner.renderText("Welcome To 1002_Tinder")
     cowsay.cow("Wanna Get Hitch? Use 1002_Tinder!")
 
+def options():
+    print "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
     print "1. List all the names, gender and age from all the profiles."
     print "2. List all the matched students of one given student B based on country."
     print "3. List the top 3 best matched students who share the most similar likes or dislikes for one given student B."
     print "4. List the top 3 best matched students based on books they like."
     print "5. List the top 3 best matched students based on the overall profile information which may include all the personal information for ranking."
     print "6. Store all the best matched students into one .csv file on the disk."
-    print "7. Exit. \n"
+    print "7. Print the banner."
+    print "8. Exit."
+    print "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
 
 
 def main():
     profiles_list = f1.FUNCTION_1(profiles=PROFILES, files=files)
     profiles_df = profiles_list.profilesDF(profiles_list.HEADERS, profiles_list.DATA)
 
-    acceptable_inputs = ["1", "2", "3", "4", "5", "6", "7"]
+    acceptable_inputs = ["1", "2", "3", "4", "5", "6", "7", "8"]
     display_ui()
+    options()
 
     # Getting student B information
     user_profile_path = raw_input("Please Import Your Profile Here: ")
@@ -45,40 +50,71 @@ def main():
     if user_input in acceptable_inputs:
 
         while user_input in acceptable_inputs:
-
             """Put this here"""
             profiles_list = f1.FUNCTION_1(profiles=PROFILES, files=files)
             profiles_df = profiles_list.profilesDF(profiles_list.HEADERS, profiles_list.DATA)
+            f2_df = f2.COUNTRY_MATCH(profiles_df, student_B_name, student_B_info).countries_matches
 
             # Option 1
             if user_input == "1":
                 print profiles_df
-                print "\n \n \n \n"
-
-                display_ui()
-                user_input = raw_input("Enter your option: ")
+                raw_input("Press Enter to continue...")
+                print "\n \n"
 
 
             # Option 2
             elif user_input == "2":
-                f2_df = f2.COUNTRY_MATCH(profiles_df, student_B_name, student_B_info).countries_matches
-
                 print f2_df
-                print "\n \n \n \n"
-
-                display_ui()
-                user_input = raw_input("Enter your option: ")
+                raw_input("Press Enter to continue...")
+                print "\n \n"
 
 
             # Option 3
+            elif user_input == "3":
+                f3_matches = f3.LIKES_DISLIKES(f2_df)  # calling the class LIKES_DISLIKESel
+                f3_matches_lst = f3_matches.temp_list  # converting dataframe to list
+
+                countLikes = f3_matches.countMatch(f3_matches_lst, student_B_info, "Likes")  # count the no. of likes
+                countDislikes = f3_matches.countMatch(f3_matches_lst, student_B_info, "Dislikes")  # count the no. of dislikes
+
+                f3_df = f3_matches.matches(countLikes, countDislikes, f3_matches_lst)
+                print f3_df.head(n=5)[["Name", "Gender", "Rank"]]
+                raw_input("Press Enter to continue...")
+                print "\n \n"
+
+
+            # Option 4
+            elif user_input == "4":
+                print "test4"
+                raw_input("Press Enter to continue...")
+
+
+            # Option 5
+            elif user_input == "5":
+                print "test5"
+                raw_input("Press Enter to continue...")
+
+
+            # Option 6
+            elif user_input == "6":
+                print "test6"
+                raw_input("Press Enter to continue...")
+
 
             # Option 7
             elif user_input == "7":
+                display_ui()
+
+
+            # Option 8
+            elif user_input == "8":
                 print "Thank You for Using 1002_Tinder!"
                 print "Have a nice day!"
+                raw_input()
                 break
 
-                # User entered an invalid input
+            options()
+            user_input = raw_input("Enter your option: ")
     else:
         print "Invalid Option Entered! Exiting Program."
         print "Thank You for Using 1002_Tinder!"
