0443e5d80ff9ce8e2ad34265be0e8bf42337447b
Roach105 <32363441+Roach105@users.noreply.github.com>
2019-02-03 15:45:05 +0800

    Update to UI

--- starting_ui.py
+++
@@ -1,9 +0,0 @@
-from pyfiglet import Figlet
-import cowsay
-
-ui_banner = Figlet(font = "graffiti")
-print ui_banner.renderText("Welcome To Zhao Tinder")
-cowsay.cow("Best Student Couple Matching!")
-
-
-profiles_folder_path = raw_input("Enter Profile Folder Path: ") 
