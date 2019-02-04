4a1b5e99dbcd492c16b2831a5f032ced576cc54b
zhaoyea <tanzhaoyea98.tzy@gmail.com>
2019-02-04 09:41:39 +0800

    remove 5k data. Wrong data

--- function2.py
+++ function2.py
@@ -1,6 +1,7 @@
 class COUNTRY_MATCH:
     def __init__(self, df, name, student_B_info):
         student_B_gender = student_B_info.Gender.values[0]
+
         student_B_country = student_B_info["Acceptable_country"].values[0].split(",")
 
         condition = df['Country'].isin(student_B_country) & (df['Name'] != name) & (df['Gender'] != student_B_gender)
