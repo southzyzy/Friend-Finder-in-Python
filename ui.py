c25b37f38882526af41ad3b4e035ac266ec1be02
Roach105 <32363441+Roach105@users.noreply.github.com>
2019-02-04 09:57:59 +0800

    Update to UI
    Allow the user to check if his/her profile exists

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
 from pyfiglet import Figlet
 import cowsay
+import sys
 import warnings, time, os
 import function1 as f1
 import function2 as f2
@@ -8,40 +9,55 @@ import student_B as sb
 
 CURENT_DIR = os.path.dirname(__file__)  # specify current directory
 PROFILES = os.path.join(CURENT_DIR, "profile/")  # locate the data profile
-files = [file for file in os.listdir(PROFILES) if file.endswith(".txt")]  # list out all the filename in profiles folder
+API_KEY = os.path.join(CURENT_DIR, "keys/")
+pro_files = [file for file in os.listdir(PROFILES) if
+             file.endswith(".txt")]  # list out all the profiles in profiles folder
+key_files = [file for file in os.listdir(API_KEY) if file.endswith(".bin")]  # list out the api-keys in keys folder
 
 
 def display_ui():
-    ui_banner = Figlet(font="graffiti")
+    ui_banner = Figlet(font = "graffiti")
     print ui_banner.renderText("Welcome To 1002_Tinder")
     cowsay.cow("Wanna Get Hitch? Use 1002_Tinder!")
 
 def options():
     print "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+    print "Options"
     print "1. List all the names, gender and age from all the profiles."
     print "2. List all the matched students of one given student B based on country."
     print "3. List the top 3 best matched students who share the most similar likes or dislikes for one given student B."
     print "4. List the top 3 best matched students based on books they like."
     print "5. List the top 3 best matched students based on the overall profile information which may include all the personal information for ranking."
     print "6. Store all the best matched students into one .csv file on the disk."
-    print "7. Print the banner."
-    print "8. Exit."
+    print "7. Exit."
     print "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
-
-
+    
 def main():
-    profiles_list = f1.FUNCTION_1(profiles=PROFILES, files=files)
+    profiles_list = f1.FUNCTION_1(profiles = PROFILES, files = pro_files)
     profiles_df = profiles_list.profilesDF(profiles_list.HEADERS, profiles_list.DATA)
-
-    acceptable_inputs = ["1", "2", "3", "4", "5", "6", "7", "8"]
-    display_ui()
-    options()
+     
+    acceptable_inputs = ["1", "2", "3", "4", "5", "6", "7"]
 
     # Getting student B information
-    user_profile_path = raw_input("Please Import Your Profile Here: ")
-
-    student_B_name = "Kevin"
+    student_B_name = sys.argv[1]
     student_B_info = sb.STUDENT_B(profiles_df, student_B_name).student_B_info
+
+    #Verifying if the user profile exist
+    temp_list = []
+
+    for name in profiles_df["Name"]:
+        temp_list.append(name)
+    
+    if student_B_name in temp_list:
+        del temp_list 
+
+    else:
+        cowsay.tux("User Profile Does Not Exist! Exiting Program!")
+        exit()
+        
+    #Display banner & Options
+    display_ui()
+    options()
 
     # Getting user to input his/her option
     user_input = raw_input("Enter your option: ")
@@ -51,8 +67,6 @@ def main():
 
         while user_input in acceptable_inputs:
             """Put this here"""
-            profiles_list = f1.FUNCTION_1(profiles=PROFILES, files=files)
-            profiles_df = profiles_list.profilesDF(profiles_list.HEADERS, profiles_list.DATA)
             f2_df = f2.COUNTRY_MATCH(profiles_df, student_B_name, student_B_info).countries_matches
 
             # Option 1
@@ -61,13 +75,11 @@ def main():
                 raw_input("Press Enter to continue...")
                 print "\n \n"
 
-
             # Option 2
             elif user_input == "2":
                 print f2_df
                 raw_input("Press Enter to continue...")
                 print "\n \n"
-
 
             # Option 3
             elif user_input == "3":
@@ -82,32 +94,23 @@ def main():
                 raw_input("Press Enter to continue...")
                 print "\n \n"
 
-
             # Option 4
             elif user_input == "4":
                 print "test4"
                 raw_input("Press Enter to continue...")
-
 
             # Option 5
             elif user_input == "5":
                 print "test5"
                 raw_input("Press Enter to continue...")
 
-
             # Option 6
             elif user_input == "6":
                 print "test6"
                 raw_input("Press Enter to continue...")
 
-
             # Option 7
             elif user_input == "7":
-                display_ui()
-
-
-            # Option 8
-            elif user_input == "8":
                 print "Thank You for Using 1002_Tinder!"
                 print "Have a nice day!"
                 raw_input()
@@ -115,9 +118,13 @@ def main():
 
             options()
             user_input = raw_input("Enter your option: ")
+
+    #User entered an invalid input 
     else:
-        print "Invalid Option Entered! Exiting Program."
-        print "Thank You for Using 1002_Tinder!"
+        print "Invalid Option Entered!" 
+        user_input = raw_input("Enter your option: ")
+        #print "Invalid Option Entered! Exiting Program."
+        #print "Thank You for Using 1002_Tinder!"
 
 
 if __name__ == '__main__':
