cb4d2f74bb64d5f03a3697756fa73c61f484fcaf
zhaoyea <tanzhaoyea98.tzy@gmail.com>
2019-02-11 19:52:11 +0800

    format horoscope.py file

--- horoscope.py
+++ horoscope.py
@@ -1,12 +1,13 @@
 import datetime
 
+
 class Horoscope:
     def __init__(self):
         self.horoscopes = {
             ((3, 21), (4, 19)): "Aries",
             ((4, 20), (5, 20)): "Taurus",
             ((5, 21), (6, 20)): "Gemini",
-            ((6, 21), (7 ,22)): "Cancer",
+            ((6, 21), (7, 22)): "Cancer",
             ((7, 23), (8, 22)): "Leo",
             ((8, 23), (9, 22)): "Virgo",
             ((9, 23), (10, 22)): "Libra",
@@ -24,7 +25,7 @@ class Horoscope:
         #     if h[0] <= dateTuple <= h[1]:
         #         return self.horoscopes[(h[0], h[1])]
 
-        return self.horoscopes[ next(iter(filter(lambda x: (x[0] <= dateTuple <= x[1]), self.horoscopes))) ]
+        return self.horoscopes[next(iter(filter(lambda x: (x[0] <= dateTuple <= x[1]), self.horoscopes)))]
 
     def get_horoscope_range(self, dateInput):
         dateTuple = (dateInput.month, dateInput.day)
@@ -34,4 +35,3 @@ class Horoscope:
         #         return h
 
         return next(iter(filter(lambda x: (x[0] <= dateTuple <= x[1]), self.horoscopes)))
-
