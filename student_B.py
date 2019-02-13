cbdf3160331a5105b58f6edee4e52182d7b2b9b0
zhaoyea <tanzhaoyea98.tzy@gmail.com>
2019-02-13 09:30:33 +0800

    improve checking on pandas dataframe

--- student_B.py
+++ student_B.py
@@ -3,8 +3,10 @@ class STUDENT_B(object):
         self.df = df
 
     def check_name(self, name):
-        if name in self.df.Name.values:
-            return self.df.loc[self.df["Name"].isin([name])]
+        name = name.lower()
+        if name in map(lambda x: x.lower(), self.df.Name.values):
+            return self.df.loc[self.df.Name.str.lower().isin([name])]
+
 
         else:
             return "Error"
