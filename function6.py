46a0817686071b46701dfbf93a360af2b0c0ef46
Roach105 <32363441+Roach105@users.noreply.github.com>
2019-02-17 18:27:24 +0800

    Commented on function6.py

--- function6.py
+++ function6.py
@@ -1,3 +1,14 @@
+"""
+Function 6:
+    1. Uses Pandas's built-in .DataFrame.to_csv method to export data from the dataframe to a .csv file 
+
+Read More:
+pandas.DataFrame.apply: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_csv.html
+"""
+
+
+
+
 class FUNCTION6(object):
     def __init__(self, final_df):
         self.df = final_df
