6669df36c15d0b11bcbc6aa6dfc62a4b29edd642
Roach105 <32363441+Roach105@users.noreply.github.com>
2019-02-17 18:35:12 +0800

    student_b.py commented

--- student_B.py
+++ student_B.py
@@ -1,3 +1,8 @@
+"""
+Student_B:
+Iterate all the dataset and attempts to determine if Student B exists within the dataset 
+"""
+
 class STUDENT_B(object):
     def __init__(self, df):
         self.df = df
