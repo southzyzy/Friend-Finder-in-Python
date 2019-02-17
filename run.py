70a5fcc4986413f97f8bbd2dfd2690c3e2684d49
Roach105 <32363441+Roach105@users.noreply.github.com>
2019-02-17 18:45:14 +0800

    run.py commented

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
+"""
+run:
+This part holds all the codes to make the Friend Finder GUI workable.
+
+The GUI runs on a webserver, WSGIserver.
+    1. Start the application's GUI on localhost. 
+
+Read More:
+WSGIserver: https://pypi.org/project/WSGIserver/
+"""
+
 from gevent import monkey
 monkey.patch_all()
 from app import app
